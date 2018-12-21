15e724fd478f7408673c2cf17ba8018a3c124d9a
aafatankit <ankitmp3@hotmail.com>
2018-12-21 13:07:33 +0530

    Login
    Modal added

--- header.php
+++ header.php
@@ -58,7 +58,7 @@
                                 <a href="Offers.php" class="nav-link"><i class="fa fa-twitch"></i> Offers</a>
                             </li>
                             <li class="nav-item">
-                                <a href="#" class="nav-link"><i class="fa fa-user"></i> Login</a>
+                                <a href="#" class="nav-link" data-toggle="modal" data-target="#myModal"><i class="fa fa-user"></i> Login</a>
                             </li>
                         </ul>
                     </div>
@@ -67,3 +67,29 @@
         
 
 	<!-- end:header-top -->
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
