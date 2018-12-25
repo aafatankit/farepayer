45ecf853a137053cdde69e7dce701c126f26b4a9
rajatbhadauria <45893758+rajatbhadauria@users.noreply.github.com>
2018-12-25 11:01:16 +0530

    update
    update

--- header.php
+++ header.php
@@ -1,27 +1,8 @@
 <!-- Start:header-top -->
 	
-            <div class="bg-primary">
-            <div class="container">
-                <div class="row">
-        
-                    <span class="col-md-3 text-white text-left"></span>
-                    <span class="col-md-5"></span>
-                    <span class="col-md-4 text-white text-right" style="font-size: 12px; margin-bottom: 3px; ">
-                        <span class="float-right"><i class="fa fa-envelope"></i> support@farepayer.com</span>
-                        <span class="float-right"><i class="fa fa-phone-square"></i> 011-45661507 &nbsp;&nbsp;</span>
-                        
-<!--
-                        <span class="row">
-                            <span class="col text-left"><i class="fa fa-envelope"></i>011-45661507</span>
-                            <span class="col text-right"><i class="fa fa-envelope"></i> support@farepayer.com</span>
-                        </span>
--->
-                    </span>
-                </div>
-            </div>
-        </div>
+            
             <nav class="navbar navbar-expand-md navbar-light bg-white sticky-top">
-                <div class="container">
+          
                     <a href="#" class="navbar-brand"><img src="images/logo.png"></a>
                     <button class="navbar-toggler" data-toggle="collapse" data-target="#navbaraId">
                         <span class="navbar-toggler-icon"></span>
