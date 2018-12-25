37e03147bd8d984b33cc9c286106bd68bbc4cd74
aafatankit <ankitmp3@hotmail.com>
2018-12-25 15:32:58 +0530

    Update search.php

--- search.php
+++ search.php
@@ -4,13 +4,13 @@ include 'connect.php';
 
     if(isset($_POST['query'])){
 //        $output = '';
-        $query = "select * from city where cityName LIKE '%".$_POST['query']."%'";
+        $query = "select * from city where CityName LIKE '%".$_POST['query']."%'";
         $result = mysqli_query($con,$query);
         $avail = mysqli_num_rows($result);
         $output = '<ul class="list-unstyled">';
         if(mysqli_num_rows($result) > 0){
             while($row=mysqli_fetch_array($result)){
-                $output = $output.'<li>'.$row['cityName'].'</li>';
+                $output .= '<li>'.$row['CityName'].'</li>';
             }
 //            for($i=0;$i<$avail;$i++){
 //                $row=mysqli_fetch_array($result);
