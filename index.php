3e674220e3dee5532efe246c8735ff79bcbf42eb
rajatbhadauria <45893758+rajatbhadauria@users.noreply.github.com>
2019-01-03 09:11:47 +0530

    upp
    upp

--- index.php
+++ index.php
@@ -368,7 +368,7 @@
 <div id="fh5co-tours" class="fh5co-section-gray">
 			<div class="container">
 				<div class="row" >
-					<div class="col-md-12 text-center heading-section" style="margin-bottom: 120px;">
+					<div class="col-md-12 text-center heading-section" style="margin-bottom: 82px;position: relative;z-index: 9;">
 						<h3><b>Hot Tours</b></h3>
 						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 					</div>
