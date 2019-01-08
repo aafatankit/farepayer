36e1fadf9422ca636e747893e3e9c015848c55be
rajatbhadauria <45893758+rajatbhadauria@users.noreply.github.com>
2019-01-08 18:46:16 +0530

    u
    u

--- Hotelsearch.php
+++ Hotelsearch.php
@@ -51,27 +51,7 @@
 <body>
 <style>
     
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-    
+
     
 /* The CSS */
 select {
@@ -123,54 +103,7 @@ label:before {
     pointer-events:none;
     display:block;
 }
-/*
-    .dropbtn {
-  background-color: #3498DB;
-  color: white;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
-}
 
-.dropbtn:hover, .dropbtn:focus {
-  background-color: #2980B9;
-}
-
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f1f1f1;
-  min-width: 160px;
-  overflow: auto;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown a:hover {background-color: #ddd;}
-
-.show {display: block;}
-*/
-
-    
-    
-
-    
-    
-    
-    
     
 	#more {display: none;}
 	.responsive {
--- REVIEWYOURBOOKING.php
+++ REVIEWYOURBOOKING.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="en"><head>
 	<meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
@@ -31,12 +30,11 @@
 	<!-- Magnific Popup -->
 	<link rel="stylesheet" href="css/magnific-popup.css">
 	<!-- Date Picker -->
-	<link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
+	
 	<!-- CS Select -->
-	<link rel="stylesheet" href="css/cs-select.css">
-	<link rel="stylesheet" href="css/cs-skin-border.css">
-	<link rel="stylesheet" type="text/css" href="engine1/style.css" />
-	<script type="text/javascript" src="engine1/jquery.js"></script>
+	
+
+
 	<link rel="stylesheet" href="css/style.css">
 
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
@@ -44,23 +42,142 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	<!-- Modernizr JS -->
- <link rel="stylesheet"  href="lightslider-master/src/css/lightslider.css"/>
+
+
+
 
 
 
 <style>
+	
+	.bs-wizard {border-bottom: solid 1px #e0e0e0; padding: 0 0 10px 0;}
+.bs-wizard > .bs-wizard-step {padding: 0; position: relative;}
+.bs-wizard > .bs-wizard-step + .bs-wizard-step {}
+.bs-wizard > .bs-wizard-step .bs-wizard-stepnum {color: #595959; font-size: 16px; margin-bottom: 5px;}
+.bs-wizard > .bs-wizard-step .bs-wizard-info {color: #999; font-size: 14px;}
+.bs-wizard > .bs-wizard-step > .bs-wizard-dot {position: absolute; width: 30px; height: 30px; display: block; background: #fbe8aa; top: 45px; left: 50%; margin-top: -15px; margin-left: -15px; border-radius: 50%;} 
+.bs-wizard > .bs-wizard-step > .bs-wizard-dot:after {content: ' '; width: 14px; height: 14px; background: #fbbd19; border-radius: 50px; position: absolute; top: 8px; left: 8px; } 
+.bs-wizard > .bs-wizard-step > .progress {position: relative; border-radius: 0px; height: 8px; box-shadow: none; margin: 20px 0;}
+.bs-wizard > .bs-wizard-step > .progress > .progress-bar {width:0px; box-shadow: none; background: #fbe8aa;}
+.bs-wizard > .bs-wizard-step.complete > .progress > .progress-bar {width:100%;}
+.bs-wizard > .bs-wizard-step.active > .progress > .progress-bar {width:50%;}
+.bs-wizard > .bs-wizard-step:first-child.active > .progress > .progress-bar {width:0%;}
+.bs-wizard > .bs-wizard-step:last-child.active > .progress > .progress-bar {width: 100%;}
+.bs-wizard > .bs-wizard-step.disabled > .bs-wizard-dot {background-color: #f5f5f5;}
+.bs-wizard > .bs-wizard-step.disabled > .bs-wizard-dot:after {opacity: 0;}
+.bs-wizard > .bs-wizard-step:first-child  > .progress {left: 50%; width: 50%;}
+.bs-wizard > .bs-wizard-step:last-child  > .progress {width: 50%;}
+.bs-wizard > .bs-wizard-step.disabled a.bs-wizard-dot{ pointer-events: none; }
+/*END Form Wizard*/
+	
+	
+	
+	
+	
+ table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+ 
+  text-align: left;
+  padding: 8px;
+}
+
+th {
+     border: solid 1px #dedede;
+}	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	.modals {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modals-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	.nopadding{margin: 0px;padding: 0px;}
 	.responsive {
   width: 100%;
-  height: auto;
+   height: 123px;
 }
-	
+
+	.myform{border: solid 1px #9d9d9d;
+    border-radius: 5px;
+	display: block;
+    width: 100%;
+    height: calc(2.25rem + 2px);
+    padding: 10px;
+    font-size: 13px;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+    transition: border-color .15s }
 	
 	
 	
 	</style>
 
 </head>
-<body>
+<body >
 
 
 
@@ -69,44 +186,276 @@
    <?php include 'header.php'; ?>
         
 	<!-- end:header-top -->
-   <h3 style="font-weight: 700;
+<div class="container"style="font-weight: 600;">
+		<div class="row bs-wizard" style="border-bottom:0;padding: 16px;border: solid 1px #d9d9d9;">
+              <div class="col-xs-2 bs-wizard-step complete">
+                  <div class="text-center bs-wizard-stepnum">Search</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
+             </div>
+              <div class="col-xs-2 bs-wizard-step complete"><!-- complete -->
+                  <div class="text-center bs-wizard-stepnum">Select Hotel</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
+               </div>
+               <div class="col-xs-2 bs-wizard-step complete">
+                  <div class="text-center bs-wizard-stepnum">Select Room</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
+            </div>
+                 <div class="col-xs-2 bs-wizard-step complete"><!-- complete -->
+                  <div class="text-center bs-wizard-stepnum">Rewiew Booking</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"style="background: #009688 !important;"></a>
+               </div>
+               <div class="col-xs-2 bs-wizard-step disabled"><!-- active -->
+                  <div class="text-center bs-wizard-stepnum">Conform </div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
+               </div>
+            </div>
+    </div>
+	
+
+ 
+ <h3 style="font-weight: 700;color: #386d9c;
     text-align: -webkit-center;
     margin-top: 23px;
     margin-bottom: 20px;"><u>REVIEW YOUR BOOKING</u></h3><br>
-<div class="row">
-	
+
+
 <div class="container">
-<div class="col-md-9">
-<div class="container"style="background:#cccccc54;">
-<h3>Guest Datails</h3></div></div>
+<div class="col-md-8">
 
-<div class="col-md-3 mtop" style="background:#cccccc54;">
-
+<div class="container"style="padding:10px;border: solid 10px #bababa42;font-size: 13px;">
+<div class="col-md-3 nopadding">
 <img src="images/hotel/hotel1.jpg" class="responsive" alt="">
-	<h3 style="">Hotel Name<br></h3>
- 	<i class="fa fa-star" style="font-size:15px;color:#FF9800;"></i>
+</div>
+<div class="col-md-9"><b>Hotel Name</b>
+<i class="fa fa-star" style="font-size:15px;color:#FF9800;"></i>
 	<i class="fa fa-star" style="font-size:15px;color:#FF9800;"></i>
 	<i class="fa fa-star" style="font-size:15px;color:#FF9800;"></i>
-	<br>
-	<p text-light style="font-size: small;">
+	<i class="fa fa-star" style="font-size:15px;color:#FF9800;"></i>
+		<p text-light style="font-size: small;">
 	<i class="fa fa-map-marker fa-2x"></i><b> Address :</b>916 Gali Chandi Wali, Main Bazaar, New Delhi, 110055, India ZipCode: 110055
 	</p>
-	<h4><b>Type of Room :</b> 4 Bed Female Dormitory</h4>
-	<h4>Amenities :Breakfast </h4></div>
-	
+	<div class="row text-center">
+	<div class="col-md-3"style="border: solid 2px #cccccc;margin: 5px;"><b>Check in</b><br>19/1/2019</div>
+	<div class="col-md-3"style="border: solid 2px #cccccc;margin: 5px;"><b>Check Out</b><br>19/1/2019</div>
+	<div class="col-md-3"style="border: solid 2px #cccccc;margin: 5px;"><b>Night</b><br>2</div>
+	<div class="col-md-3"style="border: solid 2px #cccccc;margin: 5px;"><b>Room Type</b><br>Standard Room</div>
+	<div class="col-md-3"style="border: solid 2px #cccccc;margin: 5px;"><b>Amenities</b><br>Full Breakfast</div>
+	<div class="col-md-3"style="border: solid 2px #cccccc;margin: 5px;"><b>Guest</b><br>1 Adult</div>
+		<a href="#"><p style="color: white;background: #7279c7;padding: 5px;border-radius: 4px;margin: 8px;"><i class='far fa-edit'></i>modify</p></a>
 
+<!-- Button to Open the Modal -->
+<button id="myBtns">Hotel Policy</button>
+<div id="myModals" class="modals">
+<div class="modals-content">
+    <span class="close">&times;</span>
+    <p>Some text in .</p>
+  </div>
+</div>
+
+
+<script>
+// Get the modal
+var modal = document.getElementById('myModals');
+
+var btn = document.getElementById("myBtns");
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modals) {
+    modals.style.display = "none";
+  }
+}
+</script>
+
+
+<button id="myBtn1">Hotel Policy</button>
+<div id="myModals1" class="modals">
+
+  <!-- Modal content -->
+  <div class="modals-content">
+    <span class="close1">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
+<script>
+
+var modal1 = document.getElementById('myModals1');
+
+var btn1 = document.getElementById("myBtn1");
+
+var span1 = document.getElementsByClassName("close")[0];
+
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+</script>
+
+</div>
+	
+ </div> 
+  	
+   	</div>
+   	
+   	<br><p></p>
+   	
+	</div>    
+    		
+    	
+ <div class="col-md-4"style="padding:10px;border: solid 2px #bababa42;font-size: 13px;font-weight: 600;">    
+  <h4 class="text-center"style="padding: 5px;background: #afc9e0;"><b>Price Details</b></h4>
+        
+
+
+<h5><b><u>Price Per Day :</u></b></h5>
+
+<table>
+
+  <tr>
+    <td>Day 1</td>
+    <td>2019-01-09T00:00:00</td>
+    <td> 917.64</td>
+  </tr>
+  <tr>
+    <td>Day 2</td>
+    <td>2019-03-09T00:00:00</td>
+    <td> 917.64</td>
+  </tr> 
+   <tr>
+    <td>Extra Guest Charge</td>
+    <td>(if any)</td>
+    <td> 700.64</td>
+  </tr> 
+ 
+   <tr>
+    <td>Child Charge</td>
+    <td>(if any)</td>
+    <td> 700.64</td>
+  </tr>
+  <tr style="background: #7279c7;color: white;">
+    <td><b>Total Amount</b></td>
+    <td></td>
+    <td><b>1937/-</b></td>
+   
+  </tr>
+
+</table>
+     <a href="#"><button class="btn success">Info</button></a>  
+        
+        
 	</div>
-	
-	
-<!--	<h5><b>Hotel Policy : </b>Municipality Tax: W.E.F 14th February 2018 The Kingdom of Saudi Arabia will start to charge the following Municipality Fee of 5.0% for 5 to 4-star Hotels and 2.5% for 3-star Hotels and below, across all properties in their country. Kindly inform all your guest travelling to KSA to avoid any last minute confusion.|india - land of mystries Early check out will attract full cancellation charge unless otherwise specified.|4 Bed Female Dormitory.</h5>-->
 
-	
-	
-	
-	
+
+<div class="col-md-8">
+
+<div class="container"style="
+    padding: 10px;
+    border: solid 5px #bababa42;
+    font-size: 13px;">
+    
+<h3>Guest Datails</h3>
+<br>
+  <p></p>
+  
+   <div class="row">  
+    <div class="col-md-1">
+      <label for="title">Title</label>
+   <select id="title" name="title">
+        <option value="australia">Mr.</option>
+        <option value="canada">Miss.</option>
+        <option value="australia">Mrs.</option>
+        <option value="canada">Ms.</option>
+        
+      </select>
+    </div>
+ 				<div class="col-md-4 col-lg-4">
+                        <div class="form-group">
+                            <label class="control-label">First Name</label>
+                            <input type="text" class="myform" placeholder="First name" />
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-4">
+                        <div class="form-group">
+                            <label class="control-label">Last Name</label>
+                            <input type="text" class="myform"  placeholder="Last Name"/>
+                        </div>
+                    </div>  
+                       <div class="col-md-3 col-lg-3">
+                        <div class="form-group">
+                            <label class="control-label">Contact No.</label>
+                <input type="text" class="myform"  placeholder="Mobile Number"/>
+                        </div>
+                    </div>  
+                    
+
+             
+                </div>
+                
+    <div class="row">  
+    
+ 	<div class="col-md-3 col-lg-3">
+                        <div class="form-group">
+                            <label class="control-label">Email </label>
+                            <input type="text" class="myform" placeholder="Email" />
+                        </div>
+                    </div>
+                  <div class="col-md-3 col-lg-3">
+                        <div class="form-group">
+                            <label class="control-label">Mobile Number</label>
+                            <input type="text" class="myform"  placeholder="Mobile Number"/>
+                        </div>
+                    </div>  
+                         <div class="col-md-2 col-lg-2 ">
+                        <div class="form-group">
+                            <label class="control-label">Age</label>
+                            <input type="text" class="myform"  placeholder="Age"/>
+                        </div>
+                    </div>   
+                   
+ </div>      
+    		
+    	
+     
+          
+
+</div>
+
+
 </div>
 </div>
 
+	
+	
+
+<!-- Trigger/Open The Modal -->
 
 
 <!--footer start-->	
